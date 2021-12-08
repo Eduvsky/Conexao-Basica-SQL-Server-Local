24c161e9a01869db6d9a4c6c3df25ab7afef14ce
Eduardo Skye <skyemails1@gmail.com>
2021-12-07 22:12:47 -0300

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SGBD_Practice_SQL_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listbox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Listar Filmes";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listbox
+            // 
+            this.listbox.FormattingEnabled = true;
+            this.listbox.Location = new System.Drawing.Point(12, 60);
+            this.listbox.Name = "listbox";
+            this.listbox.Size = new System.Drawing.Size(212, 238);
+            this.listbox.TabIndex = 1;
+            this.listbox.SelectedIndexChanged += new System.EventHandler(this.listbox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 306);
+            this.Controls.Add(this.listbox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listbox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SGBD_Practice_SQL_Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string path = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=
+            ""C:\Users\skyem\source\repos\SGBD Practice SQL Server\SGBD Practice SQL Server\Database.mdf"";Integrated Security=True";
+
+            string consult = "SELECT * FROM Filme";
+
+            // Cria uma conexão, um tunel para acessar o Banco de dados 
+            SqlConnection conection = new SqlConnection(path);
+            conection.Open();// <-- Aqui essa conexão / tunel é aberta
+
+            // O Construtor dessa classe recebe dois parametros, a instrução SQL, e o tunel para envia-la
+            SqlCommand comand = new SqlCommand(consult, conection);
+            SqlDataReader leitor = comand.ExecuteReader(); // <-- Usamos esse comando para atribuir o valor do retorno na variavel leitor
+
+            while(leitor.Read()) // <-- Posiciona o cursor de linhas na tabela
+            {
+                listbox.Items.Add(leitor["Nome"].ToString()); // <-- Para acessar um elemento da linha onde o leitor esta posicionado basta indicar o nome da coluna
+            }
+            leitor.Close();
+
+            while (leitor.Read())
+            {
+                listbox.Items.Add(leitor["Id"].ToString());
+            }
+
+            // Aqui são fechadas as conexões
+            leitor.Close();
+            conection.Close();
+        }
+    }
+}
